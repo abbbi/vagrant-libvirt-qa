c1fb51ddabc81c2672abfa4e39045c8814cc1467
Michael Ablassmeier <abi@grinser.de>
2020-04-19 08:22:28 +0200

    use sudo

--- vagrant-libvirt-test-cycle.sh
+++ vagrant-libvirt-test-cycle.sh
@@ -3,8 +3,8 @@
 BOXES=`vagrant status | grep libvirt | awk '{ print $1 }'`
 
 for box in $BOXES; do
-    vagrant destroy --provider=libvirt -f $box
+    sudo vagrant destroy --provider=libvirt -f $box
     echo "vagrant-libvirt distro test for $box - `date`" > $box.log
-    vagrant up --provider=libvirt $box 2>&1 | tee -a $box.log
-    vagrant halt --provider=libvirt
+    sudo vagrant up --provider=libvirt $box 2>&1 | tee -a $box.log
+    sudo vagrant halt --provider=libvirt
 done
