2dabe5c99f738cc374df7445c194b9893c7d4d76
Michael Ablassmeier <abi@grinser.de>
2020-04-19 22:10:40 +0200

    fix halt

--- vagrant-libvirt-test-cycle.sh
+++ vagrant-libvirt-test-cycle.sh
@@ -6,5 +6,5 @@ for box in $BOXES; do
     /tmp/exec/vagrant destroy -f $box
     echo "vagrant-libvirt distro test for $box - `date`" > $box.log
     /tmp/exec/vagrant up --provider=libvirt $box 2>&1 | tee -a $box.log
-    /tmp/exec/vagrant halt --provider=libvirt
+    /tmp/exec/vagrant halt 
 done
